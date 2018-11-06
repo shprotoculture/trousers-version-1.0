d29153996809a8643837c7678c7f762b8e65e54f
Nikolay <shprotoculture@yandex.ru>
2018-11-06 19:39:09 +0300

    Start the project

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'trousers',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Trousers' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    {src: '~/assets/scss/global.import.scss', lang: 'scss'}
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    
+    postcss: [
+      require('autoprefixer')({
+        browsers: ['> 5%']
+      })
+    ],
+    extractCSS: {
+      allChunks: true
+    }
+  }
+}
+
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="container">
+    <img src="http://placehold.it/640x480" alt="">
+  </section>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+
